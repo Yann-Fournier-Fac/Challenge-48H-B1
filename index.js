421059967f79dad357489fca4804d045093b6a7f
Yannpyc <yann.fournier@ynov.com>
2023-04-03 22:28:20 +0200

    add plugin

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Bouton pour activer le plugin
 document.getElementById('bouton').addEventListener('click', function() {
     document.getElementById('plugin').style.display = 'block';
+});
+
+// Bouton pour le désactiver sans modifications
+document.getElementById('bouton-croix').addEventListener('click', function() {
+    document.getElementById('plugin').style.display = 'none';
+});
+
+// Bouton pour faire les modifications
+document.getElementById('bouton-save').addEventListener('click', function() {
+    var taille = document.getElementById("input-taille").value;
+    var type = document.getElementById("input-type").value;
+    document.getElementById("tit-princ").style = "font-family: " + type + ", Helvetica, sans-serif; font-size:" +taille +";";
+    document.getElementById("tit1").style = "font-family: " + type + ", Helvetica, sans-serif; font-size:" +taille +";";
+    document.getElementById("info1").style = "font-family: " + type + ", Helvetica, sans-serif; font-size:" +taille +";";
+    document.getElementById("tit2").style = "font-family: " + type + ", Helvetica, sans-serif; font-size:" +taille +";";
+    document.getElementById("info2").style = "font-family: " + type + ", Helvetica, sans-serif; font-size:" +taille +";";
+    document.getElementById('plugin').style.display = 'none';
 });
